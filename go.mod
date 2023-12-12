f8357e62907dba0e28ee69706d18d465adeb270b
Mr-AnujShukla <aksht1211t@gmail.com>
2023-12-13 00:30:30 +0530

    added the main file and functions in it

---
+++ go.mod
@@ -0,0 +1,8 @@
+module book_management_system
+
+go 1.21.5
+
+require (
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/mattn/go-sqlite3 v1.14.18 // indirect
+)
---
+++ main.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type Book struct{
+	ID int 'json:"id"'
+	Title string 'json:"tittle"'
+	Author string 'json:"author"'
+}
+
+var db *sql.DB
+
+func initDB(){
+	var err error
+	db, err = sql.Open("sqlite3", "./books.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	createTable := `
+	CREATE TABLE IF NOT EXISTS books (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		title TEXT,
+		author TEXT
+	);
+	`
+	
+	_, err = db.Exec(createTable)
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+}
+func main() {
+	initDB()
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/books", getBooks).Methods("GET")
+	router.HandleFunc("/books/{id}", getBook).Methods("GET")
+	router.HandleFunc("/books", createBook).Methods("POST")
+	router.HandleFunc("/books/{id}", updateBook).Methods("PUT")
+	router.HandleFunc("/books/{id}", deleteBook).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	var books []Book
+	rows, err := db.Query("SELECT * FROM books")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var book Book
+		err := rows.Scan(&book.ID, &book.Title, &book.Author)
+		if err != nil {
+			log.Fatal(err)
+		}
+		books = append(books, book)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
+}
+
+func getBook(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id, _ := strconv.Atoi(params["id"])
+
+	var book Book
+	err := db.QueryRow("SELECT * FROM books WHERE id=?", id).Scan(&book.ID, &book.Title, &book.Author)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(book)
+}
+unc createBook(w http.ResponseWriter, r *http.Request) {
+	var book Book
+	json.NewDecoder(r.Body).Decode(&book)
+
+	result, err := db.Exec("INSERT INTO books (title, author) VALUES (?, ?)", book.Title, book.Author)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	id, _ := result.LastInsertId()
+	book.ID = int(id)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(book)
+}
+func updateBook(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id, _ := strconv.Atoi(params["id"])
+
+	var book Book
+	json.NewDecoder(r.Body).Decode(&book)
+
+	_, err := db.Exec("UPDATE books SET title=?, author=? WHERE id=?", book.Title, book.Author, id)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(book)
+}
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id, _ := strconv.Atoi(params["id"])
+
+	_, err := db.Exec("DELETE FROM books WHERE id=?", id)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
